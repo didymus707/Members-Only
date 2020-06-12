c653ce68a07c20992d5bd2d7ae48527c294e2260
Adewale Orotayo <didymus7007@gmail.com>
2020-06-12 17:58:10 +0100

    Mimicked Devise Authenticate_user! in Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def authenticate_user
+    self.redirect_to root_path
+    flash[:notice]= 'Please Sign in first or Sign up'
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user, except: [:index]
 
   def index
     @posts = Post.all.order('created_at DESC')
