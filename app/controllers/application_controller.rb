6c801a82155b141542452e6ca4b340519c6be5fc
Adewale Orotayo <didymus7007@gmail.com>
2020-06-12 18:26:15 +0100

    is_logged_in helper method used in place of devise user_signed_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+  helper_method :is_logged_in?
+
+  def is_logged_in?
+    !current_user.nil?
+  end
+
   def authenticate_user
     self.redirect_to root_path
-    flash[:notice]= 'Please Sign in first or Sign up'
+    flash[:notice] = "Please Sign in first or Sign up if you don't have an account"
   end
 end
