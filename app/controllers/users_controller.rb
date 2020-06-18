6a86d906cf218863705754441b6161377da4ea76
Adewale Orotayo <didymus7007@gmail.com>
2020-06-18 16:57:44 +0100

    switched to postgres

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
@@ -7,6 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      flash[:success]
       redirect_to root_path
     else
       render 'new'
@@ -24,7 +26,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+
+  end
 
   private
 
